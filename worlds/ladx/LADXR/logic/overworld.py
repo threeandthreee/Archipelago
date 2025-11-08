a833a8d23672298097b5f7cd6a51a49199a044f8
threeandthreee <alex@3and3.dev>
2025-11-07 21:13:27 -0500

    stg

--- worlds/ladx/LADXR/logic/overworld.py
+++ worlds/ladx/LADXR/logic/overworld.py
@@ -6,8 +6,7 @@ from ..worldSetup import ENTRANCE_INFO
 
 class World:
     def __init__(self, options, world_setup, r):
-        self.overworld_entrance = {}
-        self.indoor_location = {}
+        self.entrances = {}
 
         mabe_village = Location("Mabe Village")
         Location().add(HeartPiece(0x2A4)).connect(mabe_village, r.bush)  # well
@@ -658,33 +657,33 @@ class World:
         self.windfish = windfish
 
     def _addEntrance(self, name, outside, inside, requirement):
-        assert name not in self.overworld_entrance, "Duplicate entrance: %s" % name
+        assert name not in self.entrances, "Duplicate entrance: %s" % name
         assert name in ENTRANCE_INFO
-        self.overworld_entrance[name] = EntranceExterior(outside, requirement)
-        self.indoor_location[name] = inside
+        self.entrances[name] = EntranceExterior(outside, requirement)
+        self.entrances[f"{name}:inside"] = EntranceExterior(inside, None)
 
     def _addEntranceRequirement(self, name, requirement):
-        assert name in self.overworld_entrance
-        self.overworld_entrance[name].addRequirement(requirement)
+        assert name in self.entrances
+        self.entrances[name].addRequirement(requirement)
 
     def _addEntranceRequirementEnter(self, name, requirement):
-        assert name in self.overworld_entrance
-        self.overworld_entrance[name].addEnterRequirement(requirement)
+        assert name in self.entrances
+        self.entrances[name].addEnterRequirement(requirement)
 
     def _addEntranceRequirementExit(self, name, requirement):
-        assert name in self.overworld_entrance
-        self.overworld_entrance[name].addExitRequirement(requirement)
+        assert name in self.entrances
+        self.entrances[name].addExitRequirement(requirement)
 
     def updateIndoorLocation(self, name, location):
-        assert name in self.indoor_location
-        assert self.indoor_location[name] is None
-        self.indoor_location[name] = location
+        name = f"{name}:inside"
+        assert name in self.entrances, name
+        assert self.entrances[name].location is None
+        self.entrances[name].location = location
 
 
 class DungeonDiveOverworld:
     def __init__(self, options, r):
-        self.overworld_entrance = {}
-        self.indoor_location = {}
+        self.entrances = {}
 
         start_house = Location("Start House").add(StartItem())
         Location().add(ShopItem(0)).connect(start_house, OR(COUNT("RUPEES", 200), SWORD))
@@ -702,23 +701,23 @@ class DungeonDiveOverworld:
         windfish = Location("Windfish").connect(nightmare, AND(MAGIC_POWDER, SWORD, OR(BOOMERANG, BOW)))
 
         self.start = start_house
-        self.overworld_entrance = {
-            "d1": EntranceExterior(start_house, None),
-            "d2": EntranceExterior(start_house, None),
-            "d3": EntranceExterior(start_house, None),
-            "d4": EntranceExterior(start_house, None),
-            "d5": EntranceExterior(start_house, FLIPPERS),
-            "d6": EntranceExterior(start_house, None),
-            "d7": EntranceExterior(start_house, None),
-            "d8": EntranceExterior(start_house, None),
-            "d0": EntranceExterior(start_house, None),
+        self.entrances = {
+            "d1:inside": EntranceExterior(start_house, None),
+            "d2:inside": EntranceExterior(start_house, None),
+            "d3:inside": EntranceExterior(start_house, None),
+            "d4:inside": EntranceExterior(start_house, None),
+            "d5:inside": EntranceExterior(start_house, FLIPPERS),
+            "d6:inside": EntranceExterior(start_house, None),
+            "d7:inside": EntranceExterior(start_house, None),
+            "d8:inside": EntranceExterior(start_house, None),
+            "d0:inside": EntranceExterior(start_house, None),
         }
         self.egg = egg
         self.nightmare = nightmare
         self.windfish = windfish
 
     def updateIndoorLocation(self, name, location):
-        self.indoor_location[name] = location
+        self.entrances[f"{name}:inside"].location = location
 
 
 class EntranceExterior:
--- worlds/ladx/LADXR/worldSetup.py
+++ worlds/ladx/LADXR/worldSetup.py
@@ -1,7 +1,7 @@
 from .patches import enemies, bingo
 from .locations.items import *
 from .entranceInfo import ENTRANCE_INFO
-
+from . import logic
 
 
 MULTI_CHEST_OPTIONS = [MAGIC_POWDER, BOMB, MEDICINE, RUPEES_50, RUPEES_20, RUPEES_100, RUPEES_200, RUPEES_500, SEASHELL, GEL, ARROWS_10, SINGLE_ARROW]
@@ -24,7 +24,8 @@ start_locations = [
 
 class WorldSetup:
     def __init__(self):
-        self.entrance_mapping = {k: k for k in ENTRANCE_INFO.keys()}
+        self.entrance_mapping = {k: f"{k}:inside" for k in ENTRANCE_INFO.keys()}
+        self.entrance_mapping.update({f"{k}:inside": k for k in ENTRANCE_INFO.keys()})
         self.boss_mapping = list(range(9))
         self.miniboss_mapping = {
             # Main minibosses
@@ -39,52 +40,165 @@ class WorldSetup:
         self.bingo_goals = None
         self.multichest = RUPEES_20
         self.map = None  # Randomly generated map data
+        self.inside_to_outside = True
+        self.keep_two_way = True
+        self.one_to_one = True
 
     def getEntrancePool(self, settings, connectorsOnly=False):
         entrances = []
 
         if connectorsOnly:
-            if settings.entranceshuffle in ("advanced", "expert", "insanity"):
+            if settings.entranceshuffle in {"split", "mixed", "wild", "chaos", "insane", "madness"}:
                 entrances = [k for k, v in ENTRANCE_INFO.items() if v.type == "connector"]
-            
+            entrances += [f"{k}:inside" for k in entrances]
             return entrances
 
         if settings.dungeonshuffle and settings.entranceshuffle == "none":
             entrances = [k for k, v in ENTRANCE_INFO.items() if v.type == "dungeon"]
-        if settings.entranceshuffle in ("simple", "advanced", "expert", "insanity"):
+        if settings.entranceshuffle in {"simple", "split", "mixed", "wild", "chaos", "insane", "madness"}:
             types = {"single"}
             if settings.tradequest:
                 types.add("trade")
-            if settings.entranceshuffle in ("expert", "insanity"):
+            if settings.shufflejunk:
                 types.update(["dummy", "trade"])
-            if settings.entranceshuffle in ("insanity",):
+            if settings.shuffleannoying:
                 types.add("insanity")
+            if settings.shufflewater:
+                types.add("water")
             if settings.randomstartlocation:
                 types.add("start")
             if settings.dungeonshuffle:
                 types.add("dungeon")
+            if settings.entranceshuffle in {"mixed", "wild", "chaos", "insane", "madness"}:
+                types.add("connector")
             entrances = [k for k, v in ENTRANCE_INFO.items() if v.type in types]
 
+        entrances += [f"{k}:inside" for k in entrances]
         return entrances
 
+
+    def _pair_equal_length_lists(self, outsides, insides, rnd):
+        mapping = {}
+        n = min(len(outsides), len(insides))
+        for i in range(n):
+            o = outsides[i]
+            i_ = insides[i]
+            mapping[o] = i_
+            mapping[i_] = o
+        return mapping
+
+    def _two_way_shuffle(self, entrances, rnd):
+        mapping = {}
+        all_entrances = list(entrances)
+        rnd.shuffle(all_entrances)
+        n = len(all_entrances)
+        i = 0
+        while i + 1 < n:
+            a, b = all_entrances[i], all_entrances[i + 1]
+            mapping[a] = b
+            mapping[b] = a
+            i += 2
+        if n % 2:
+            last = all_entrances[-1]
+            choice = rnd.choice(all_entrances[:-1])
+            mapping[last] = choice
+            mapping[choice] = last
+        return mapping
+
+    def _decoupled_shuffle(self, sources, targets, rnd):
+        mapping = {}
+        n = min(len(sources), len(targets))
+        for i in range(n):
+            mapping[sources[i]] = targets[i]
+        return mapping
+
+    def _randomizeEntrances(self, rnd, settings, entrancePool):
+        entrances = list(entrancePool)
+        outsides = [e for e in entrances if not e.endswith(":inside")]
+        insides  = [e for e in entrances if e.endswith(":inside")]
+
+        rnd.shuffle(outsides)
+        rnd.shuffle(insides)
+
+        mapping = {}
+
+        mode = settings.entranceshuffle
+        if mode == "simple":
+            mapping.update(self._pair_equal_length_lists(outsides, insides, rnd))
+        elif mode == "split":
+            singles = [e for e in entrances if ENTRANCE_INFO[e.replace(":inside", "")].type == "single"]
+            connectors = [e for e in entrances if ENTRANCE_INFO[e.replace(":inside", "")].type == "connector"]
+            mapping.update(self._pair_equal_length_lists(
+                [x for x in singles if not x.endswith(":inside")],
+                [x for x in singles if x.endswith(":inside")],
+                rnd
+            ))
+            mapping.update(self._pair_equal_length_lists(
+                [x for x in connectors if not x.endswith(":inside")],
+                [x for x in connectors if x.endswith(":inside")],
+                rnd
+            ))
+        elif mode == "mixed":
+            mapping.update(self._pair_equal_length_lists(outsides, insides, rnd))
+        elif mode == "wild":
+            mapping.update(self._two_way_shuffle(entrances, rnd))
+        elif mode == "chaos":
+            mapping.update(self._decoupled_shuffle(outsides, insides, rnd))
+        elif mode == "insane":
+            mapping.update(self._decoupled_shuffle(entrances, entrances, rnd))
+        else:  # none / vanilla
+            mapping = VANILLA_ENTRANCE_MAPPING.copy()
+
+        self.entrance_mapping = mapping
+        self._checkEntranceRules()
+
+    def pickEntrances(self, settings, rnd):
+        if settings.entranceshuffle == "none":
+            if settings.randomstartlocation:
+                start_location = start_locations[rnd.randrange(len(start_locations))]
+                if start_location != "start_house":
+                    # simple swap for starting house
+                    self.entrance_mapping = dict(self.entrance_mapping)  # make mutable copy
+                    self.entrance_mapping[start_location] = "start_house:inside"
+                    self.entrance_mapping["start_house:inside"] = start_location
+                    self.entrance_mapping["start_house"] = f"{start_location}:inside"
+                    self.entrance_mapping[f"{start_location}:inside"] = "start_house"
+            return
+
+        entrancePool = self.getEntrancePool(settings)
+        self._randomizeEntrances(rnd, settings, entrancePool)
+
+
+    def _checkEntranceRules(self):
+        mapping = self.entrance_mapping
+        keys = list(mapping.keys())
+        values = list(mapping.values())
+
+        # inside-to-outside
+        if self.inside_to_outside:
+            for k, v in zip(keys, values):
+                if k.endswith(":inside"):
+                    assert not v.endswith(":inside"), f"inside-to-outside rule violated: {k}->{v}"
+                else:
+                    assert v.endswith(":inside"), f"inside-to-outside rule violated: {k}->{v}"
+
+        # two-way check (bidirectional) without following cycles
+        if self.keep_two_way:
+            for k in keys:
+                v = mapping[k]
+                if v in mapping:
+                    # only check if v is a key, don't touch mapping[v] if not
+                    assert mapping.get(v) == k, f"keep-two-way rule violated: {k}->{v}"
+
+        # one-to-one
+        if self.one_to_one:
+            found = set()
+            for v in values:
+                assert v not in found, f"one-on-one rule violated: {v}"
+                found.add(v)
+
+
     def randomize(self, settings, rnd):
-        if settings.overworld == "dungeondive":
-            self.entrance_mapping = {"d%d" % (n): "d%d" % (n) for n in range(9)}
-        if settings.randomstartlocation and settings.entranceshuffle == "none":
-            start_location = start_locations[rnd.randrange(len(start_locations))]
-            if start_location != "start_house":
-                self.entrance_mapping[start_location] = "start_house"
-                self.entrance_mapping["start_house"] = start_location
-        
-        entrances = self.getEntrancePool(settings)
-        for entrance in entrances.copy():
-            self.entrance_mapping[entrance] = entrances.pop(rnd.randrange(len(entrances)))
-
-        # Shuffle connectors among themselves
-        entrances = self.getEntrancePool(settings, connectorsOnly=True)
-        for entrance in entrances.copy():
-            self.entrance_mapping[entrance] = entrances.pop(rnd.randrange(len(entrances)))
-
         if settings.boss != "default":
             values = list(range(9))
             if settings.heartcontainers:
@@ -123,6 +237,13 @@ class WorldSetup:
             self.bingo_goals = bingo.randomizeGoals(rnd, settings)
 
         self.multichest = rnd.choices(MULTI_CHEST_OPTIONS, MULTI_CHEST_WEIGHTS)[0]
+
+        self.inside_to_outside = settings.entranceshuffle not in {"wild", "insane", "madness"}
+        self.keep_two_way = settings.entranceshuffle not in {"chaos", "insane", "madness"}
+        self.one_to_one = settings.entranceshuffle not in {"madness"}
+        self.base_logic = logic.Logic(settings, world_setup=self)
+        self.pickEntrances(settings, rnd)
+
 
     def loadFromRom(self, rom):
         import patches.overworld
