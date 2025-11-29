46eabe4f43f62a8c432a83bfe247b1371f6d7cc7
threeandthreee <alex@3and3.dev>
2025-11-29 04:32:56 -0500

    Revert "dumping other worlds, hopefully i dont regret"
    This reverts commit 2a30374e29904563aec75767a7a7762ed7027fd6.

--- worlds/LauncherComponents.py
+++ worlds/LauncherComponents.py
@@ -232,6 +232,20 @@ components: List[Component] = [
     Component('Text Client', 'CommonClient', 'ArchipelagoTextClient', func=launch_textclient,
               description="Connect to a multiworld using the text client."),
     Component('LttP Adjuster', 'LttPAdjuster'),
+    # Ocarina of Time
+    Component('OoT Client', 'OoTClient',
+              file_identifier=SuffixIdentifier('.apz5')),
+    Component('OoT Adjuster', 'OoTAdjuster'),
+    # TLoZ
+    Component('Zelda 1 Client', 'Zelda1Client', file_identifier=SuffixIdentifier('.aptloz')),
+    # ChecksFinder
+    Component('ChecksFinder Client', 'ChecksFinderClient'),
+    # Zillion
+    Component('Zillion Client', 'ZillionClient',
+              file_identifier=SuffixIdentifier('.apzl')),
+
+    # MegaMan Battle Network 3
+    Component('MMBN3 Client', 'MMBN3Client', file_identifier=SuffixIdentifier('.apbn3')),
 
     Component("Export Datapackage", func=export_datapackage, component_type=Type.TOOL),
 ]
