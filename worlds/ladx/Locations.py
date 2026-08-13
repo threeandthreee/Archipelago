8d7710177671627c596102a40f5487fa97ee1184
threeandthreee <alex@3and3.dev>
2026-08-13 08:31:26 -0400

    init

--- worlds/ladx/Locations.py
+++ worlds/ladx/Locations.py
@@ -1,5 +1,5 @@
-from BaseClasses import Region, Entrance, Location, CollectionState
-import typing
+from BaseClasses import Region, Entrance, Location
+from typing import Dict, Set
 
 from .LADXR.checkMetadata import checkMetadataTable
 from .Common import *
@@ -25,7 +25,7 @@ links_awakening_dungeon_names = [
 def meta_to_name(meta):
     return f"{meta.name} ({meta.area})"
 
-def get_location_name_groups() -> typing.Dict[str, typing.Set[str]]:
+def get_location_name_groups() -> Dict[str, Set[str]]:
     groups = {
         "Instrument Pedestals": {
             "Full Moon Cello (Tail Cave)",
--- worlds/ladx/__init__.py
+++ worlds/ladx/__init__.py
@@ -1,7 +1,7 @@
 import binascii
 import dataclasses
 import os
-import typing
+from typing import cast, Any, Dict, ClassVar
 import logging
 import re
 import struct
@@ -105,7 +105,7 @@ class LinksAwakeningSettings(settings.Group):
 
 
     rom_file: RomFile = RomFile(RomFile.copy_to)
-    rom_start: typing.Union[RomStart, bool] = True
+    rom_start: RomStart | bool = True
     gfx_mod_file: GfxModFile = GfxModFile()
 
 class LinksAwakeningWebWorld(WebWorld):
@@ -119,7 +119,7 @@ class LinksAwakeningWebWorld(WebWorld):
     )]
     theme = "ocean"
     option_groups = ladx_option_groups
-    options_presets: typing.Dict[str, typing.Dict[str, typing.Any]] = {
+    options_presets: Dict[str, Dict[str, Any]] = {
         "Keysanity": {
             "shuffle_nightmare_keys": "any_world",
             "shuffle_small_keys": "any_world",
@@ -139,7 +139,7 @@ class LinksAwakeningWorld(World):
 
     options_dataclass = LinksAwakeningOptions
     options: LinksAwakeningOptions
-    settings: typing.ClassVar[LinksAwakeningSettings]
+    settings: ClassVar[LinksAwakeningSettings]
     topology_present = True  # show path to required location checks in spoiler
 
     # ID of first item and location, could be hard-coded but code may be easier
@@ -168,7 +168,7 @@ class LinksAwakeningWorld(World):
 
     ladxr_settings: LADXRSettings
     ladxr_logic: LADXRLogic
-    ladxr_itempool: LADXRItemPool
+    ladxr_itempool: Dict
 
     multi_key: bytearray
 
@@ -258,8 +258,8 @@ class LinksAwakeningWorld(World):
         
         self.multiworld.completion_condition[self.player] = lambda state: state.has("An Alarm Clock", player=self.player)
 
-    def create_item(self, item_name: str):
-        return LinksAwakeningItem(self.item_name_to_data[item_name], self, self.player)
+    def create_item(self, name: str):
+        return LinksAwakeningItem(self.item_name_to_data[name], self, self.player)
 
     def create_event(self, event: str):
         return Item(event, ItemClassification.progression, None, self.player)
@@ -285,7 +285,11 @@ class LinksAwakeningWorld(World):
             for _ in range(count):
                 item = self.create_item(item_name)
 
-                if not self.options.tradequest and isinstance(item.item_data, TradeItemData):
+                if (
+                    not self.options.tradequest 
+                    and isinstance(item.item_data, TradeItemData)
+                    and item.item_data.vanilla_location
+                ):
                     location = self.multiworld.get_location(item.item_data.vanilla_location, self.player)
                     location.place_locked_item(item)
                     location.show_in_spoiler = False
@@ -300,6 +304,7 @@ class LinksAwakeningWorld(World):
                         # TODO: we should be able to pinpoint the region we want, save a lookup table please
                         found = False
                         for r in self.multiworld.get_regions(self.player):
+                            r = cast(LinksAwakeningRegion, r)
                             if r.dungeon_index != item.item_data.dungeon_index:
                                 continue
                             for loc in r.locations:
@@ -334,10 +339,12 @@ class LinksAwakeningWorld(World):
        
         self.dungeon_locations_by_dungeon = [[], [], [], [], [], [], [], [], []]     
         for r in self.multiworld.get_regions(self.player):
+            r = cast(LinksAwakeningRegion, r)
             # Set aside dungeon locations
             if r.dungeon_index:
                 self.dungeon_locations_by_dungeon[r.dungeon_index - 1] += r.locations
                 for location in r.locations:
+                    location = cast(LinksAwakeningLocation, location)
                     # Don't place dungeon items on pit button chest, to reduce chance of the filler blowing up
                     # TODO: no need for this if small key shuffle
                     if location.name == "Pit Button Chest (Tail Cave)" or location.item:
@@ -516,6 +523,7 @@ class LinksAwakeningWorld(World):
     def generate_output(self, output_directory: str):
         self.rom_item_placements = []
         for r in self.multiworld.get_regions(self.player):
+            r = cast(LinksAwakeningRegion, r)
             for loc in r.locations:
                 if isinstance(loc, LinksAwakeningLocation):
                     assert(loc.item)
@@ -556,7 +564,7 @@ class LinksAwakeningWorld(World):
     def generate_multi_key(self):
         return bytearray(self.random.getrandbits(8) for _ in range(10)) + self.player.to_bytes(2, 'big')
 
-    def modify_multidata(self, multidata: dict):
+    def modify_multidata(self, multidata):
         multidata["connect_names"][binascii.hexlify(self.multi_key).decode()] = multidata["connect_names"][self.player_name]
 
     def collect(self, state, item: Item) -> bool:
